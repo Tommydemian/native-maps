ad4f5b45992af18f0616e4d5177927d8d1f033ea
tomas gil amoedo <tomasgilamoedo@tomass-MBP.fibertel.com.ar>
2023-11-09 01:19:25 -0300

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <SafeAreaView>
+      <Text>Page</Text>
+    </SafeAreaView>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
